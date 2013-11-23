7360ce8acbb9b408d0b2108fdb2f14e3e277ac18
Joshua Davey <josh@joshuadavey.com>
2013-11-23 13:36:57 -0600

    Add example graph-walking

--- src/kevin/core.clj
+++ src/kevin/core.clj
@@ -1,7 +1,38 @@
 (ns kevin.core
-  )
+  (:require [kevin.loader :refer [conn uri]]
+            [datomic.api :as d :refer [q db]]
+            [clojure.set :refer [union]]))
 
-(defn foo
-  "I don't do a whole lot."
-  [x]
-  (println x "Hello, World!"))
+(defn actor-name->eid
+  "d is database value
+  name is the actor's name"
+  [d name]
+  (ffirst (q '[:find ?e
+               :in $ ?name
+               :where [?e :actor/name ?name]]
+             d name)))
+
+(defn immediate-connections
+  "d is database value
+  eid is actor's entity id"
+  [d eid]
+  (let [e (d/entid d eid)]
+    (->> (q '[:find ?c
+              :in $ ?eid
+              :where
+              [?eid :movies ?m]
+              [?c :movies ?m]] d e)
+         (map first)
+         set)))
+
+(defn connections-at-degree
+  "d is database value
+  eid is actor's entity id
+  deg is degree of separation (> 0)"
+  [d deg eid]
+  (let [e (d/entid d eid)]
+    (if (zero? deg)
+      #{e}
+      (->> (immediate-connections d e)
+           (pmap (partial connections-at-degree d (dec deg)))
+           (apply union)))))
