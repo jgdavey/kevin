84b6251f1013378310eaca47809fb532f8d421e3
Joshua Davey <josh@joshuadavey.com>
2013-11-26 20:58:59 -0600

    Update to latest datomic

--- project.clj
+++ project.clj
@@ -4,9 +4,8 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [com.datomic/datomic-pro "0.8.4260"]]
+                 [com.datomic/datomic-pro "0.9.4314"]]
   :profiles {:dev {:source-paths ["dev"]
                    :dependencies [[org.clojure/tools.namespace "0.2.4"]
                                   [org.clojure/java.classpath "0.2.0"]]}}
-  :jvm-opts ["-Xmx2g"]
   )
