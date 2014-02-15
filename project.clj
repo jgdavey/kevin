f424ad69b041878324da4a6be6596c08b5497511
Joshua Davey <josh@joshuadavey.com>
2014-02-15 15:47:36 -0600

    Bump to latest version of datomic

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [com.datomic/datomic-pro "0.9.4497"]
+                 [com.datomic/datomic-pro "0.9.4556"]
                  [hiccup "1.0.4"]
                  [ring-server "0.3.1"]
                  [compojure "1.1.6"]]
