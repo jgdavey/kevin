5b4edb4924a50a9d543fd3524f6880c9434d63d0
Joshua Davey <josh@joshuadavey.com>
2014-03-06 15:52:06 -0600

    Minify CSS on build

--- config.rb
+++ config.rb
@@ -54,7 +54,7 @@ set :images_dir, 'images'
 # Build-specific configuration
 configure :build do
   # For example, change the Compass output style for deployment
-  # activate :minify_css
+  activate :minify_css
 
   # Minify Javascript on build
   # activate :minify_javascript
