09a8934c0abe47615cbe65dd65b3e7d3bb428ce5
Joshua Davey <josh@joshuadavey.com>
2014-03-19 16:26:55 -0500

    Cleanup indentation

--- dev/user.clj
+++ dev/user.clj
@@ -71,112 +71,112 @@
 
 (comment
 
-  (reset)
+(reset)
 
-  ;; number of movies, total
-  (time (q '[:find (count ?e) :where [?e :movie/title]]
-           (-> system :db :conn db)))
+;; number of movies, total
+(time (q '[:find (count ?e) :where [?e :movie/title]]
+          (-> system :db :conn db)))
 
-  ;; number of movies with actors
-  (time
-    (let [d (-> system :db :conn db)]
-      (q '[:find (count ?e)
-           :where
-           [?e :movie/title]
-           [_ :actor/movies ?e]]
-         d)))
-
-  ;; number of movies with no actors
-  (time
-    (let [d (-> system :db :conn db)
-          movies (q '[:find ?e :where [?e :movie/title]] d)]
-      (->> (map (fn [[id]] (d/entity d id)) movies)
-           (remove (fn [e] (:actor/_movies e)))
-           count)))
-
-  ;; retract video games
+;; number of movies with actors
+(time
   (let [d (-> system :db :conn db)]
-    (->> (q '[:find ?e ?name
-              :where
-              [?e :movie/title ?name]] d)
-         (filter (fn [[e n]] (not= -1 (.indexOf n "(VG)"))))
-         (mapv (fn [[e _]] [:db.fn/retractEntity e]))
-         (d/transact (-> system :db :conn))
-         (deref)
-         ))
+    (q '[:find (count ?e)
+          :where
+          [?e :movie/title]
+          [_ :actor/movies ?e]]
+        d)))
 
-  ;; zipper
+;; number of movies with no actors
+(time
   (let [d (-> system :db :conn db)
-        a (actor-name->eid d "Barth, Clayton")
-        actor-name (partial eid->actor-name d)
-        kevin (actor-name->eid d "Bacon, Kevin (I)")
-        tree (zipper d a)]
-    (time (some (fn [n] (when (= kevin n) n)) tree)))
+        movies (q '[:find ?e :where [?e :movie/title]] d)]
+    (->> (map (fn [[id]] (d/entity d id)) movies)
+          (remove (fn [e] (:actor/_movies e)))
+          count)))
+
+;; retract video games
+(let [d (-> system :db :conn db)]
+  (->> (q '[:find ?e ?name
+            :where
+            [?e :movie/title ?name]] d)
+        (filter (fn [[e n]] (not= -1 (.indexOf n "(VG)"))))
+        (mapv (fn [[e _]] [:db.fn/retractEntity e]))
+        (d/transact (-> system :db :conn))
+        (deref)
+        ))
+
+;; zipper
+(let [d (-> system :db :conn db)
+      a (actor-name->eid d "Barth, Clayton")
+      actor-name (partial eid->actor-name d)
+      kevin (actor-name->eid d "Bacon, Kevin (I)")
+      tree (zipper d a)]
+  (time (some (fn [n] (when (= kevin n) n)) tree)))
 
 
-  ;; bi-directional bfs
-  (def from-bfs
-    (let [d (-> system :db :conn db)
-          clay (actor-name->eid d "Barth, Clayton")
-          kevin (actor-name->eid d "Bacon, Kevin (I)")
-          neighbor-fn (partial neighbors d)]
-      (time (bidirectional-bfs clay kevin neighbor-fn)))
-    )
-
-  ;; queue-based search
-  (let [d (-> system :db :conn db)
-        clay (actor-name->eid d "Barth, Clayton")
-        kevin (actor-name->eid d "Bacon, Kevin (I)")
-        neighbor-fn (partial neighbors d)
-        actor-name (partial actor-or-movie-name d)]
-    (time (map actor-name (bfs clay kevin neighbor-fn))))
-
-
-  ;; query engine search (3 degrees)
-  (let [d (-> system :db :conn db)
-        clay (actor-name->eid d "Barth, Clayton")
-        kevin (actor-name->eid d "Bacon, Kevin (I)")
-        actor-name (partial eid->actor-name d)]
-    (time (q '[:find ?actor ?m1 ?target
-                           :in $ % ?actor ?target
-                           :where (acted-with ?actor ?m1 _)
-                           (acted-with ?m1 ?target _)]
-                         d acted-with-rules clay kevin)))
-
-  ;; query engine search (4 degrees)
+;; bi-directional bfs
+(def from-bfs
   (let [d (-> system :db :conn db)
         clay (actor-name->eid d "Barth, Clayton")
         kevin (actor-name->eid d "Bacon, Kevin (I)")
-        actor-name (partial eid->actor-name d)]
-    (time (map (partial map actor-name)
-               (q '[:find ?actor ?m1 ?m2 ?target
-                           :in $ % ?actor ?target
-                           :where (acted-with ?actor ?m1 _)
-                           (acted-with ?m1 ?m2 _)
-                           (acted-with ?m2 ?target _)]
-                         d acted-with-rules clay kevin))))
+        neighbor-fn (partial neighbors d)]
+    (time (bidirectional-bfs clay kevin neighbor-fn)))
+  )
 
-  ;; using path from rule
-  (def from-path
-    (let [d (-> system :db :conn db)
-          clay (actor-name->eid d "Barth, Clayton")
-          kevin (actor-name->eid d "Bacon, Kevin (I)")
-          ename  (partial actor-or-movie-name d)]
-      (time (set (map (fn [[p]] (concat p (list kevin)))
-                    (q '[:find ?path
-                        :in $ % ?actor ?target
-                        :where
-                        (acted-with-3 ?actor ?target ?path)]
-                      d acted-with-rules clay kevin)))))
-    )
+;; queue-based search
+(let [d (-> system :db :conn db)
+      clay (actor-name->eid d "Barth, Clayton")
+      kevin (actor-name->eid d "Bacon, Kevin (I)")
+      neighbor-fn (partial neighbors d)
+      actor-name (partial actor-or-movie-name d)]
+  (time (map actor-name (bfs clay kevin neighbor-fn))))
 
 
-  ;; export movies and actors
+;; query engine search (3 degrees)
+(let [d (-> system :db :conn db)
+      clay (actor-name->eid d "Barth, Clayton")
+      kevin (actor-name->eid d "Bacon, Kevin (I)")
+      actor-name (partial eid->actor-name d)]
+  (time (q '[:find ?actor ?m1 ?target
+                          :in $ % ?actor ?target
+                          :where (acted-with ?actor ?m1 _)
+                          (acted-with ?m1 ?target _)]
+                        d acted-with-rules clay kevin)))
+
+;; query engine search (4 degrees)
+(let [d (-> system :db :conn db)
+      clay (actor-name->eid d "Barth, Clayton")
+      kevin (actor-name->eid d "Bacon, Kevin (I)")
+      actor-name (partial eid->actor-name d)]
+  (time (map (partial map actor-name)
+              (q '[:find ?actor ?m1 ?m2 ?target
+                          :in $ % ?actor ?target
+                          :where (acted-with ?actor ?m1 _)
+                          (acted-with ?m1 ?m2 _)
+                          (acted-with ?m2 ?target _)]
+                        d acted-with-rules clay kevin))))
+
+;; using path from rule
+(def from-path
   (let [d (-> system :db :conn db)
-        reducer (fn [map [k v]] (assoc map k (conj (get map k []) v)))
-        actor-map (->> (kevin.expunge/actor-names "data/movies-small.list" d)
-                     (reduce reducer {}))]
-      (spit "resources/sample.edn" (with-out-str (pr actor-map))))
+        clay (actor-name->eid d "Barth, Clayton")
+        kevin (actor-name->eid d "Bacon, Kevin (I)")
+        ename  (partial actor-or-movie-name d)]
+    (time (set (map (fn [[p]] (concat p (list kevin)))
+                  (q '[:find ?path
+                      :in $ % ?actor ?target
+                      :where
+                      (acted-with-3 ?actor ?target ?path)]
+                    d acted-with-rules clay kevin)))))
+  )
+
+
+;; export movies and actors
+(let [d (-> system :db :conn db)
+      reducer (fn [map [k v]] (assoc map k (conj (get map k []) v)))
+      actor-map (->> (kevin.expunge/actor-names "data/movies-small.list" d)
+                    (reduce reducer {}))]
+    (spit "resources/sample.edn" (with-out-str (pr actor-map))))
 
 
 ;; quick n dirty profiling
