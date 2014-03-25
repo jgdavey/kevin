73ad02229b691175e1fc9316dadcefc866d3508d
Joshua Davey <josh@joshuadavey.com>
2014-03-25 08:51:19 -0500

    Hash generated assets

--- config.rb
+++ config.rb
@@ -48,21 +48,14 @@ require 'phil'
 # end
 
 set :css_dir, 'stylesheets'
-
 set :js_dir, 'javascripts'
-
 set :images_dir, 'images'
 
 # Build-specific configuration
 configure :build do
-  # For example, change the Compass output style for deployment
   activate :minify_css
-
-  # Minify Javascript on build
-  # activate :minify_javascript
-
-  # Enable cache buster
-  # activate :asset_hash
+  activate :minify_javascript
+  activate :asset_hash
 
   # Use relative URLs
   # activate :relative_assets
