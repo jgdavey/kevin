c227899c73aab393cefebe5eff9976bfd7dbe6f7
Joshua Davey <josh@joshuadavey.com>
2014-11-07 11:23:07 -0600

    Upgrade datomic, other dependencies

--- project.clj
+++ project.clj
@@ -4,17 +4,17 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/tools.nrepl "0.2.3"]
+                 [org.clojure/tools.nrepl "0.2.6"]
                  [org.clojure/core.async "0.1.303.0-886421-alpha"]
-                 [commons-codec "1.6"]
+                 [commons-codec "1.9"]
                  [hiccup "1.0.5"]
                  [enlive "1.1.5"]
-                 [environ "0.5.0"]
-                 [lib-noir "0.8.4" :exclusions [org.clojure/core.cache ring org.clojure/tools.reader]]
+                 [environ "1.0.0"]
+                 [lib-noir  "0.9.4" :exclusions  [compojure com.fasterxml.jackson.core/jackson-core ring org.clojure/tools.reader org.clojure/core.cache]]
                  [ring-server "0.3.1"]
-                 [ring "1.3.0"]
+                 [ring "1.3.1"]
                  [compojure "1.1.8"]]
-  :plugins [[lein-ring "0.8.10"]
+  :plugins [[lein-ring "0.8.13"]
             [lein-beanstalk "0.2.7"]]
   :aws {:access-key ~(System/getenv "AWS_ACCESS_KEY_ID")
         :secret-key ~(System/getenv "AWS_SECRET_KEY") }
@@ -23,9 +23,9 @@
          :destroy kevin.system/destroy }
   :profiles {:production
              {:ring {:open-browser? false :stacktraces? false :auto-reload? false}
-              :dependencies [[com.datomic/datomic-pro "0.9.4815.12"]]}
+              :dependencies [[com.datomic/datomic-pro "0.9.5067" :exclusions [joda-time]]]}
              :dev {:source-paths ["dev"]
-                   :dependencies [[com.datomic/datomic-free "0.9.4815.12"]
+                   :dependencies [[com.datomic/datomic-free "0.9.5067" :exclusions [joda-time]]
                                   [org.clojure/tools.namespace "0.2.4"]
                                   [org.clojure/java.classpath "0.2.2"]
                                   [javax.servlet/servlet-api "2.5"]
