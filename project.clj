2c36affb50377e9d58bbf5d048062e02b4316717
Joshua Davey <josh@joshuadavey.com>
2015-02-15 13:44:31 -0600

    Update datomic

--- project.clj
+++ project.clj
@@ -23,9 +23,9 @@
          :destroy kevin.system/destroy }
   :profiles {:production
              {:ring {:open-browser? false :stacktraces? false :auto-reload? false}
-              :dependencies [[com.datomic/datomic-pro "0.9.5067" :exclusions [joda-time]]]}
+              :dependencies [[com.datomic/datomic-pro "0.9.5130" :exclusions [joda-time]]]}
              :dev {:source-paths ["dev"]
-                   :dependencies [[com.datomic/datomic-free "0.9.5067" :exclusions [joda-time]]
+                   :dependencies [[com.datomic/datomic-free "0.9.5130" :exclusions [joda-time]]
                                   [org.clojure/tools.namespace "0.2.4"]
                                   [org.clojure/java.classpath "0.2.2"]
                                   [javax.servlet/servlet-api "2.5"]
